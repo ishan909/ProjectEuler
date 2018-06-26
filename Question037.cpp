8eb90d8ba9251c9a23fe3a789dfcd9156ceecbd9
Ishan Patel <32139516+ishan909@users.noreply.github.com>
2018-06-25 19:53:01 -0500

    Fixed prime numbers bug

--- Question037.cpp
+++ Question037.cpp
@@ -11,7 +11,7 @@ bool prime(unsigned n) {
     if (n == 1) {
         return false;
     }
-    for (int i = 2; i < sqrt(n) + 1; i++) {
+    for (int i = 2; i <= sqrt(n); i++) {
         if (n % i == 0) {
             return false;
         }
